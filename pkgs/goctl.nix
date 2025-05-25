a7b8520337c8722e81598a89f3263780d617c8ef
KurikoMoe <kurikomoe@gmail.com>
2025-05-25 12:13:05 +0800

    exp: fix goModule

--- pkgs/goctl.nix
+++ pkgs/goctl.nix
@@ -1,19 +1,33 @@
 {
-  pkgs,
   lib,
+  system,
   sources,
+  buildGoModule,
+  fetchFromGitHub,
   ...
 }: let
   res = sources.goctl;
+  # nixpkgs_go_121 = fetchFromGitHub {
+  #   owner = "NixOS";
+  #   repo = "nixpkgs";
+  #   rev = "a71323f68d4377d12c04a5410e214495ec598d4c";
+  #   hash = "sha256-pl8PLUQLigZgFZrafIXs2djne5kboimK6MYsZN8Ywtg=";
+  # };
+  #
+  # pkgs_go_121 = import nixpkgs_go_121 { inherit system; };
+  #
+  # buildGoModule = pkgs_go_121.buildGoModule;
 in
-  pkgs.buildGoModule rec {
+  buildGoModule rec {
     inherit (res) pname version src;
 
     proxyVendor = true;
     sourceRoot = "source/tools/goctl";
 
     preBuild = ''
+      go version
       go mod tidy
+      go mod vendor
     '';
 
     doCheck = false;
