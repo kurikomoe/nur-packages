110e927b9a5f7280c619789e3f3a456d6c4fab4c
kurikomoe <kurikomoe@users.noreply.github.com>
2025-08-01 10:25:42 +0000

    Auto update

--- _sources/generated.nix
+++ _sources/generated.nix
@@ -100,7 +100,7 @@
       repo = "nix-vscode-extensions";
       rev = "master";
       fetchSubmodules = false;
-      sha256 = "sha256-Na6JjalzCBQx5aiZMa0QYuFU9T0FOuWSKbHnslXWln4=";
+      sha256 = "sha256-tirCA/d4v/+yyjsNHbK0mEtxfeG3SxjhTPsV9SUrb/s=";
     };
   };
   pwndbg = {
